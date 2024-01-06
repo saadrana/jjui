ecfacde69a7dcefc1cd9635f9da67d854f0c52e0
Saad Rana <40745451+saadrana@users.noreply.github.com>
2024-01-06 15:39:05 +0900

    Add cargo fmt settings

--- src/app.rs
+++ src/app.rs
@@ -55,18 +55,18 @@ pub fn update(model: &mut Model, msg: Message) -> Option<Message> {
             if model.counter > 50 {
                 return Some(Message::Reset);
             }
-        }
+        },
         Message::Decrement => {
             model.counter -= 1;
             if model.counter < -50 {
                 return Some(Message::Reset);
             }
-        }
+        },
         Message::Reset => model.counter = 0,
         Message::Quit => {
             // You can handle cleanup and exit here
             model.running_state = RunningState::Done;
-        }
+        },
     };
     None
 }
